d19a62ffd0f3dc4167dba1aeddcffaae44813dfc
ninadoe <nila_drng@yahoo.de>
2026-06-16 13:01:46 +0200

    First project set up with test method from Spring tutorial.

---
+++ src/test/java/com/nnd/recipe_app_java/RecipeAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnd.recipe_app_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
