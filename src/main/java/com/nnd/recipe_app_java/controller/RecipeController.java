2444df67389aea86eddc3cd27ef8f20783873abb
ninadoe <nila_drng@yahoo.de>
2026-06-19 15:21:18 +0200

    Adjusting getRecipe() due to the removal of the Recipe builder. Initial POST endpoint to test the Jackson compability of Recipe.

--- src/main/java/com/nnd/recipe_app_java/controller/RecipeController.java
+++ src/main/java/com/nnd/recipe_app_java/controller/RecipeController.java
@@ -3,18 +3,26 @@ package com.nnd.recipe_app_java.controller;
 import com.nnd.recipe_app_java.model.MealType;
 import com.nnd.recipe_app_java.model.Recipe;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-// test endpoint
 @RestController
 public class RecipeController {
+    // test get endpoint
     @GetMapping("/testRecipe")
-    public Recipe getRecipe(){
-        Recipe dummyRecipe = Recipe.builder()
-                .name("Pizza Margarita")
-                .portions(2)
-                .mealType(MealType.Main)
-                .build();
+    public Recipe getRecipe() {
+        System.out.println("GET HIT");
+        Recipe dummyRecipe = new Recipe();
+        dummyRecipe.setName("Pizza Margarita");
+        dummyRecipe.setPortions(2);
+        dummyRecipe.setMealType(MealType.Main);
         return dummyRecipe;
+    }
+
+    // test post endpoint
+    @PostMapping("/newRecipe")
+    public Recipe createRecipe(@RequestBody Recipe recipe) {
+        return recipe;
     }
 }
