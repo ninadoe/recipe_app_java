3c9c1f8cb951aa0c24cafc819c41b66a9c969560
ninadoe <nila_drng@yahoo.de>
2026-07-01 11:56:52 +0200

    feat: add a temporal idCounter, add exception handling for missing recipes, add method for updating recipes doc: add method descriptions

--- src/main/java/com/nnd/recipe_app_java/service/RecipeService.java
+++ src/main/java/com/nnd/recipe_app_java/service/RecipeService.java
@@ -1,47 +1,79 @@
 package com.nnd.recipe_app_java.service;
 
+import com.nnd.recipe_app_java.exceptions.RecipeNotFoundException;
 import com.nnd.recipe_app_java.model.Recipe;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class RecipeService {
 
+    // in-memory solution as long as there is no database:
     private final ArrayList<Recipe> recipeCollection = new ArrayList<>();
+    // later the database will generate ID:
+    private Long idCounter = 1L;
 
-    // createRecipe
+    /**
+     * Saves the given recipe in the recipeCollection and returns it.
+     */
     public Recipe createRecipe(Recipe recipe) {
+        recipe.setId(idCounter);
+        idCounter++;
         recipeCollection.add(recipe);
         return recipe;
     }
 
-    // findAllRecipes
+    /**
+     * Returns all recipes.
+     */
     public ArrayList<Recipe> findAllRecipes() {
         return recipeCollection;
     }
 
-    // findRecipeById
-    public Recipe findRecipeById(int id) {
+    /**
+     * Returns the recipe with the specified ID.
+     *
+     * @throws RecipeNotFoundException if no recipe with the given ID exists
+     */
+    public Recipe findRecipeById(Long id) {
         for (Recipe recipe : recipeCollection) {
             if (recipe.getId() == id) {
                 return recipe;
             }
         }
-        return null;
+        throw new RecipeNotFoundException(id);
     }
 
-    // updateRecipe
-
-    // deleteRecipeById
-    public Recipe deleteRecipeById(int id) {
+    /**
+     * Updates and returns the updated version of the recipe with the specified ID.
+     *
+     * @throws RecipeNotFoundException if no recipe with the given ID exists
+     */
+    public Recipe updateRecipeById(Long id, Recipe updatedRecipe)   {
         for (Recipe recipe : recipeCollection) {
             if (recipe.getId() == id) {
-                recipeCollection.remove(id);
+                recipeCollection.remove(recipe);
+                updatedRecipe.setId(id);
+                recipeCollection.add(updatedRecipe);
+                return updatedRecipe;
+            }
+        }
+        throw new RecipeNotFoundException(id);
+    }
+
+    /**
+     * Returns the recipe with the specified ID and deletes it from the recipeCollection.
+     *
+     * @throws RecipeNotFoundException if no recipe with the given ID exists
+     */
+    public Recipe deleteRecipeById(Long id) {
+        for (Recipe recipe : recipeCollection) {
+            if (recipe.getId() == id) {
+                recipeCollection.remove(recipe);
                 return recipe;
             }
         }
-        return null;
+        throw new RecipeNotFoundException(id);
     }
 }
